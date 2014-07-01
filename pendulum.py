The constraint and constraint gradients are generating correct functions.

pendulum.py
@@ -2,12 +2,12 @@
 
 """This script demonstrates an attempt at identifying the controller for a
 two link inverted pendulum on a cart by direct collocation. I collect
-"measured" data from the system by simulating it with a known opimtal
+"measured" data from the system by simulating it with a known optimal
 controller under the influence of random lateral force perturbations. I then
 form the optimization problem such that we minimize the error in the model's
-simulated outputs with the measured outputs. The optimizer searches for the
-best set of gains (which are unknown) that reproduce the motion and ensure
-the dynamics are valid.
+simulated outputs with respect to the measured outputs. The optimizer
+searches for the best set of controller gains (which are unknown) that
+reproduce the motion and ensure the dynamics are valid.
 
 Dependencies this runs with:
 
@@ -15,17 +15,20 @@ Dependencies this runs with:
     scipy 0.14.1
     sympy 0.7.5
     matplotlib 1.3.1
-    pydy HEAD of master
+    pydy 0.2.1
 
 """
 
+# standard lib
 from collections import OrderedDict
 
+# external
 import numpy as np
 import sympy as sym
 from scipy.interpolate import interp1d
 from scipy.linalg import solve_continuous_are
 from scipy.integrate import odeint
+from scipy.sparse import lil_matrix
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib.patches import Rectangle
@@ -621,9 +624,10 @@ def discretize(eoms, states, specified, interval='h'):
     """
     xi, xp, si, h = discrete_symbols(states, specified, interval=interval)
 
-    euler = [(i - p) / h for i, p in zip(xi, xp)]
+    euler_formula = [(i - p) / h for i, p in zip(xi, xp)]
 
-    eoms = eoms.subs(dict(zip([s.diff() for s in states], euler)))
+    # Note : The Derivative's must be substituted before the symbols.
+    eoms = eoms.subs(dict(zip([s.diff() for s in states], euler_formula)))
 
     eoms = eoms.subs(dict(zip(states + specified, xi + si)))
 
@@ -731,9 +735,40 @@ def general_constraint(eom_vector, state_syms, specified_syms,
     return constraints
 
 
-def constraint_func(gen_con_func, num_time_steps, num_states,
-                    interval_value, constant_syms, specified_syms, fixed_constants,
-                    fixed_specified):
+def constraint_func(func, num_time_steps, num_states,
+                    interval_value, constant_syms, specified_syms,
+                    fixed_constants, fixed_specified):
+    """Returns a function that evaluates all of the constraints.
+
+    Parameters
+    ----------
+    func : function
+        A function that takes the full parameter set an evaulates the
+        constraint functions or the gradient of the contraint functions.
+    num_time_steps : integer
+        The number of time steps.
+    num_states : integer
+        The number of states in the system.
+    interval_value : float
+        The interval between the time steps.
+    constant_syms : list of sympy.Symbols
+        A list of all the constants in system constraint equations.
+    specified_syms : list of sympy.Functions
+        A list of all the discrete specified inputs.
+    fixed_constants : dictionary
+        A map of all the system constants which are not free optimization
+        parameters to their fixed values.
+    fixed_specified : dictionary
+        A map of all the system's discrete specified inputs that are not
+        free optimization parameters to their fixed values.
+
+    Returns
+    -------
+    optimization_func : function
+        A function which returns constraint values given the system's free
+        parameters.
+
+    """
 
     num_free_specified = len(specified_syms) - len(fixed_specified)
 
@@ -768,43 +803,205 @@ def constraint_func(gen_con_func, num_time_steps, num_states,
 
         free_states = free[:len_states].reshape((num_states, num_time_steps))
 
-        free_specified = free[len_states:len_states + len_specified].reshape((num_free_specified, num_time_steps))
-        all_specified = build_fixed_free(specified_syms, fixed_specified, free_specified)
+        free_specified = free[len_states:len_states + len_specified]
+        free_specified = free_specified.reshape((num_free_specified,
+                                                 num_time_steps))
+        all_specified = build_fixed_free(specified_syms, fixed_specified,
+                                         free_specified)
 
         free_constants = free[len_states + len_specified:]
 
-        all_constants = build_fixed_free(constant_syms, fixed_constants, free_constants)
+        all_constants = build_fixed_free(constant_syms, fixed_constants,
+                                         free_constants)
 
-        return gen_con_func(free_states, all_specified, all_constants, interval_value)
+        return func(free_states, all_specified, all_constants, interval_value)
 
     return constraints
 
 
+def general_gradient(eom_vector, state_syms, specified_syms,
+                     constant_syms, free_constants):
+    """Returns a function that evaluates the constraints.
+
+    Parameters
+    ----------
+    discrete_eom_vec : sympy.Matrix, shape(n, 1)
+        A column vector containing the discrete symbolic expressions of the
+        n constraints.
+    state_syms : list of sympy.Functions
+        The n functions of time representing the system's states.
+    specified_syms : list of sympy.Functions
+        The m functions of time representing the system's specified inputs.
+    constant_syms : list of sympy.Symbols
+        The b symbols representing the system's specified inputs.
+    free_constants : list of sympy.Symbols
+        The p symbols which are a subset of constant_syms that will be free
+        to vary in the optimization.
+
+    Returns
+    -------
+    constraints : function
+        A function which returns the numerical values of the constraints at
+        time points 2,...,N.
+
+    """
+    xi_syms, xp_syms, si_syms, h = discrete_symbols(state_syms, specified_syms)
+
+    # The free parameters are always the n * (N - 1) state values and the
+    # user's specified unknown model constants, so the base Jacobian needs
+    # to be taken with respect to the ith, and ith - 1 states, and the free
+    # model constants.
+    partials = xi_syms + xp_syms + free_constants
+
+    # The arguments to the Jacobian function are all free symbols in the
+    # matrix expression.
+    args = xi_syms + xp_syms + si_syms + constant_syms + [h]
+
+    modules = ({'ImmutableMatrix': np.array}, 'numpy')
+
+    jac = sym.lambdify(args, eom_vector.jacobian(partials), modules=modules)
+
+    # jac is now a function that takes arguments that are made up of all the
+    # variables in the discretized equations of motion. It will be used to
+    # build the sparse constraint gradient matrix. This Jacobian function
+    # returns the non-zero elements needed to build the sparse constraint
+    # gradient.
+
+    num_free_constants = len(free_constants)
+
+    def constraints_gradient(state_values, specified_values,
+                             constant_values, interval_value):
+        """Returns a sparse matrix of constraint gradient given all of the
+        unknowns in the equations of motion over the 2, ..., N time steps.
+
+        Parameters
+        ----------
+        states : ndarray, shape(n, N)
+            The array of n states through N time steps.
+        specified_values : ndarray, shape(m, N) or shape(N,)
+            The array of m specified inputs through N time steps.
+        constant_values : ndarray, shape(b,)
+            The array of b constants.
+        interval_value : float
+            The value of the dicretization time interval.
+
+        Returns
+        -------
+        constraints_gradient : scipy.sparse.csr_matrix, shape(2 * (N-1), n * N + p)
+            A compressed sparse row matrix containing the gradient of the
+            constraints where the constaints are along the rows and the free
+            parameters are along the columns.
+
+        """
+
+        if state_values.shape[0] < 2:
+            raise ValueError('There should always be at least two states.')
+
+        x_current = state_values[:, 1:]  # n x N - 1
+        x_previous = state_values[:, :-1]  # n x N - 1
+
+        num_states = state_values.shape[0]  # n
+        num_time_steps = state_values.shape[1]  # N
+
+        gradient_matrix = lil_matrix((2 * (num_time_steps - 1),
+                                      num_states * num_time_steps +
+                                      num_free_constants))
+
+        # Now loop through the N - 1 constraints to compute the non-zero
+        # entries to the gradient matrix (the partials for n states will be
+        # computed at each iteration).
+
+        for i in range(num_time_steps - 1):
+            # n: num_states
+            # m: num_specified
+            # p: num_free_constants
+
+            xi = x_current[:, i]  # len(n)
+            xp = x_previous[:, i]  # len(n)
+            if len(specified_values.shape) < 2:
+                si = specified_values[i]  # len(m)
+            else:
+                si = specified_values[:, i]  # len(m)
+
+            args = np.hstack((xi, xp, si, constant_values, interval_value))
+
+            non_zero_derivatives = jac(*args)  # n x (2*n+p), p is len(free_constants)
+
+            # the states repeat every N - 1 constraints
+            # row_idxs = [0 * (N - 1), 1 * (N - 1),  2 * (N - 1),  n * (N - 1)]
+
+            row_idxs = [j * (num_time_steps - 1) + i
+                        for j in range(num_states)]
+
+            # The derivative columns are in this order:
+            # [x1i, x2i, ..., xni, x1p, x2p, ..., xnp, p1, ..., pp]
+            # So we need to map them to the correct column.
+
+            # first row, the columns indices mapping is:
+            # [1, N + 1, ..., N - 1] : [x1p, x1i, 0, ..., 0]
+            # [0, N, ..., 2 * (N - 1)] : [x2p, x2i, 0, ..., 0]
+            # [-p:] : p1,..., pp  the free constants
+
+            # i=0: [1, N + 1, 0, N + 0, n * N:n * N + p]
+            # i=1: [2, N + 2, 1, N + 1, n * N:n * N + p]
+            # i=2: [3, N + 3, 2, N + 2, n * N:n * N + p]
+
+            col_idxs = [i + 1, i + num_time_steps + 1, i, i + num_time_steps]
+            col_idxs += [num_states * num_time_steps + j
+                         for j in range(num_free_constants)]
+
+            substitute_matrix(gradient_matrix, row_idxs, col_idxs,
+                              non_zero_derivatives)
+
+        return gradient_matrix.tocsr()
+
+    return constraints_gradient
 
 
-###def dkkd():
-###
-    ###all_symbols = set.union(*[i.free_symbols for i in discrete_eom_vector])
-    ###jac = discrete_eom_vector.jacobian(all_symbols)
-###
-    ###xi_syms, xp_syms, si_syms, h = discrete_symbols(state_syms, specified_syms)
-###
-    ###args = [x for x in xi_syms] + [x for x in xp_syms]
-    ###args += [s for s in si_syms] + constant_syms + [h]
-###
-    ###modules = ({'ImmutableMatrix': np.array}, 'numpy')
-    ###f = sym.lambdify(args, jac, modules=modules)
-###
-    ###gradient = np.zeros(N - 1, N + par)
-###
-    ###for each constraint: # N - 1 constraints
-        ###values = np.hstack()
-        ###res = f(*values)
-###
-        #### stick result into big matrix of zeros
-        ###gradient[...] = res[...]
-###
-        #### or stick into a scipy sparse matrix
+def substitute_matrix(matrix, row_idxs, col_idxs, sub_matrix):
+    """Returns the matrix with the values given by the row and column
+    indices with those in the sub-matrix.
+
+    Parameters
+    ----------
+    matrix : ndarray, shape(n, m)
+        A matrix (i.e. 2D array).
+    row_idxs : array_like, shape(p<=n,)
+        The row indices which designate which entries should be replaced by
+        the sub matrix entries.
+    col_idxs : array_like, shape(q<=m,)
+        The column indices which designate which entries should be replaced
+        by the sub matrix entries.
+    sub_matrix : ndarray, shape(p, q)
+        A matrix of values to substitute into the specified rows and
+        columns.
+
+    Notes
+    -----
+    This makes a copy of the sub_matrix, so if it is large it may be slower
+    than a more optimal implementation.
+
+    Examples
+    --------
+
+    >>> a = np.zeros((3, 4))
+    >>> sub = np.arange(4).reshape((2, 2))
+    >>> substitute_matrix(a, [1, 2], [0, 2], sub)
+    array([[ 0.,  0.,  0.,  0.],
+           [ 0.,  0.,  1.,  0.],
+           [ 2.,  0.,  3.,  0.]])
+
+    """
+
+    assert sub_matrix.shape == (len(row_idxs), len(col_idxs))
+
+    row_idx_permutations = np.repeat(row_idxs, len(col_idxs))
+    col_idx_permutations = np.array(list(col_idxs) * len(row_idxs))
+
+    matrix[row_idx_permutations, col_idx_permutations] = sub_matrix.flatten()
+
+    return matrix
+
 
 if __name__ == "__main__":
 

test_pendulum.py
@@ -1,7 +1,29 @@
 import numpy as np
+from scipy import sparse
 from sympy import symbols, Function, Matrix, simplify
 
 import pendulum
+
+
+def test_substitute_matrix():
+
+    A = np.arange(1, 13, dtype=float).reshape(3, 4)
+    sub = np.array([[21, 22], [23, 24]])
+    new_A = pendulum.substitute_matrix(A, [1, 2], [0, 2], sub)
+    expected = np.array([[1, 2, 3, 4],
+                         [21, 6, 22, 8],
+                         [23, 10, 24, 12]], dtype=float)
+
+    np.testing.assert_allclose(new_A, expected)
+
+    A = sparse.lil_matrix(np.zeros((3, 4)))
+    sub = np.array([[21, 22], [23, 24]])
+    new_A = pendulum.substitute_matrix(A, [1, 2], [0, 2], sub)
+    expected = np.array([[0, 0, 0, 0],
+                         [21, 0, 22, 0],
+                         [23, 0, 24, 0]], dtype=float)
+
+    np.testing.assert_allclose(new_A.todense(), expected)
 
 
 def test_discrete_symbols():
@@ -80,7 +102,7 @@ def test_discretize():
     assert simplify(discrete_eoms - expected) == Matrix([0, 0])
 
 
-def test_general_constraint_function():
+def test_general_constraint():
 
     t, h = symbols('t, h')
     v, x = symbols('v, x', cls=Function)
@@ -99,7 +121,7 @@ def test_general_constraint_function():
     eom_vector = Matrix([m * (vi - vp) / h + c * vi + k * xi + fi,
                          (xi - xp) / h - vi])
 
-    constrain = pendulum.general_constraint_function(eom_vector, states, specified,
+    constrain = pendulum.general_constraint(eom_vector, states, specified,
                                              [m, c, k])
 
     state_values = np.array([[1, 2, 3, 4],
@@ -141,6 +163,8 @@ def test_constraint_function():
 
     states = [v, x]
     specified = [f]
+    constants = [m, c, k]
+    free_constants = [k]
 
     xi, vi, xp, vp, fi = symbols('xi, vi, xp, vp, fi')
 
@@ -148,18 +172,23 @@ def test_constraint_function():
                          (xi - xp) / h - vi])
 
     general_constrain = pendulum.general_constraint(eom_vector, states, specified,
-                                                     [m, c, k])
+                                                    constants)
 
+    gradient = pendulum.general_gradient(eom_vector, states, specified,
+                                         constants, free_constants)
     num_time_steps = 4
     num_states = 2
     interval_value = 0.01
     fixed_constants = {m: 1.0, c: 2.0}
     fixed_specified = {fi: np.array([2, 2, 2, 2])}
 
+    specified_syms = [fi]
+
     # you pass in the general constraint function with all the fixed values
-    constrain = pendulum.constraint_func(general_constrain,
-            num_time_steps, num_states, interval_value, [m, c, k], [fi], fixed_constants,
-            fixed_specified)
+    constrain = pendulum.constraint_func(general_constrain, num_time_steps,
+                                         num_states, interval_value, constants,
+                                         specified_syms, fixed_constants,
+                                         fixed_specified)
 
     free = np.array([1, 2, 3, 4, 5, 6, 7, 8, 3.0])
 
@@ -188,6 +217,74 @@ def test_constraint_function():
 
     np.testing.assert_allclose(result, expected)
 
+    constrain = pendulum.constraint_func(gradient, num_time_steps,
+            num_states, interval_value, constants, specified_syms, fixed_constants,
+            fixed_specified)
+
+    result = constrain(free)
+
+    x = state_values[1]
+
+    expected_gradient = np.array(
+        [[-m / h, c + m / h, 0, 0, 0, k, 0, 0, x[1]],
+         [0, -m / h, c + m / h, 0, 0, 0, k, 0, x[2]],
+         [0, 0, -m / h, c + m / h, 0, 0, 0, k, x[3]],
+         [0, -1, 0, 0, -1 / h, 1 / h, 0, 0, 0],
+         [0, 0, -1, 0, 0, -1 / h, 1 / h, 0, 0],
+         [0, 0, 0, -1, 0, 0, -1 / h, 1 / h, 0]])
+
+    np.testing.assert_allclose(result.todense(), expected_gradient)
+
+
+def test_general_gradient():
+
+    t, h = symbols('t, h')
+    v, x = symbols('v, x', cls=Function)
+    m, c, k = symbols('m, c, k')
+    f = symbols('f', cls=Function)
+
+    v = v(t)
+    x = x(t)
+    f = f(t)
+
+    states = [v, x]
+    specified = [f]
+    constants = [m, c, k]
+    free_constants = [k]
+
+    xi, vi, xp, vp, fi = symbols('xi, vi, xp, vp, fi')
+
+    eom_vector = Matrix([m * (vi - vp) / h + c * vi + k * xi + fi,
+                         (xi - xp) / h - vi])
+
+    gradient = pendulum.general_gradient(eom_vector, states, specified,
+                                         constants, free_constants)
+
+    state_values = np.array([[1, 2, 3, 4],   # v
+                             [5, 6, 7, 8]])  # x
+    specified_values = np.array([2, 2, 2, 2])
+    constant_values = np.array([1.0, 2.0, 3.0])
+
+    x = state_values[1]
+    m, c, k = constant_values
+    h = 0.01
+
+    result = gradient(state_values, specified_values, constant_values, h)
+
+    # jacobian of eom_vector wrt vi, xi, xp, vp, k
+    # [     vi,  xi,   vp,   xp,  k]
+    # [c + m/h,   k, -m/h,    0, xi]
+    # [     -1, 1/h,    0, -1/h,  0]
+
+    expected_gradient = np.array(
+        [[-m / h, c + m / h, 0, 0, 0, k, 0, 0, x[1]],
+         [0, -m / h, c + m / h, 0, 0, 0, k, 0, x[2]],
+         [0, 0, -m / h, c + m / h, 0, 0, 0, k, x[3]],
+         [0, -1, 0, 0, -1 / h, 1 / h, 0, 0, 0],
+         [0, 0, -1, 0, 0, -1 / h, 1 / h, 0, 0],
+         [0, 0, 0, -1, 0, 0, -1 / h, 1 / h, 0]])
+
+    np.testing.assert_allclose(result.todense(), expected_gradient)
 
 
 def test_constraint_gradient():