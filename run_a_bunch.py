f85f838b75310f9d6fab171d14780cc5c362203e
Jason K. Moore <moorepants@gmail.com>
2014-07-30 15:13:43 -0700

    Updated to Identifier class.'

--- run_a_bunch.py
+++ run_a_bunch.py
@@ -1,18 +1,20 @@
-from pendulum import run_identification
+from pendulum import Identifier
 
 num_links = 1
 duration = 5.0
 init_type = 'known'
 
 for sample_rate in [50.0, 75.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0]:
-    run_identification(num_links, duration, sample_rate, init_type, False, False)
+    identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+    identifier.identify()
 
 num_links = 1
 duration = 5.0
 init_type = 'close'
 
 for sample_rate in [50.0, 75.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0]:
-    run_identification(num_links, duration, sample_rate, init_type, False, False)
+    identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+    identifier.identify()
 
 num_links = 1
 duration = 5.0
@@ -20,14 +22,16 @@ init_type = 'random'
 
 for i in range(10):
     for sample_rate in [50.0, 75.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0]:
-        run_identification(num_links, duration, sample_rate, init_type, False, False)
+        identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+        identifier.identify()
 
 num_links = 1
 sample_rate = 100.0
 init_type = 'close'
 
 for duration in [1.0, 5.0, 10.0, 40.0, 60.0, 90.0, 120.0, 180.0, 240.0]:
-    run_identification(num_links, duration, sample_rate, init_type, False, False)
+    identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+    identifier.identify()
 
 num_links = 2
 sample_rate = 100.0
@@ -35,7 +39,8 @@ init_type = 'random'
 
 for i in range(10):
     for duration in [60.0, 90.0, 120.0, 180.0, 240.0]:
-        run_identification(num_links, duration, sample_rate, init_type, False, False)
+        identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+        identifier.identify()
 
 num_links = 3
 sample_rate = 100.0
@@ -43,4 +48,5 @@ init_type = 'random'
 
 for i in range(10):
     for duration in [60.0, 90.0, 120.0, 180.0, 240.0]:
-        run_identification(num_links, duration, sample_rate, init_type, False, False)
+        identifier = Identifier(num_links, duration, sample_rate, init_type, False, False)
+        identifier.identify()
