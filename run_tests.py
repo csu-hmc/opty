7944a52b67d193c87c799220459f68ec2d39e313
Jason K. Moore <moorepants@gmail.com>
2014-09-27 23:35:50 -0400

    Removed run_tests.py as nosetests now work.

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import test_pendulum
-import test_utils
-
-
-for module in [test_pendulum, test_utils]:
-    for test in dir(module):
-        if test.startswith('test_'):
-            getattr(module, test)()
