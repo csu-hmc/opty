42f55f7e7cb48a593c50f071633ece075cf56c49
Jason K. Moore <moorepants@gmail.com>
2014-10-01 23:33:12 -0400

    Added animation to pendulum_swing_up.py.

--- pendulum_swing_up.py
+++ pendulum_swing_up.py
@@ -13,11 +13,15 @@ from collections import OrderedDict
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 from direct_collocation import ConstraintCollocator, Problem
 
-duration = 3.0
+target_angle = 5 * np.pi
+duration = 5.0
 num_nodes = 100
+save_animation = False
+
 interval_value = duration / (num_nodes - 1)
 
 I, m, g, d, t = sym.symbols('I, m, g, d, t')
@@ -45,7 +49,7 @@ def obj_grad(free):
 
 theta, omega = sym.symbols('theta, omega', cls=sym.Function)
 instance_constraints = (theta(0.0),
-                        theta(duration) - sym.pi,
+                        theta(duration) - target_angle,
                         omega(0.0),
                         omega(duration))
 
@@ -77,9 +81,43 @@ con_violations = prob.con(solution)
 fig, axes = plt.subplots(5)
 
 axes[0].plot(time, angle)
+axes[0].set_ylabel('Angle [rad]')
 axes[1].plot(time, rate)
+axes[1].set_ylabel('Angular Rate [rad]')
 axes[2].plot(time, torque)
+axes[2].set_ylabel('Torque [Nm]')
+axes[2].set_xlabel('Time [S]')
 axes[3].plot(con_violations)
 axes[4].plot(prob.obj_value)
+
+# Animation
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+time_template = 'time = {:0.1f}s'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+def init():
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+
+def animate(i):
+    x = [0, par_map[d] * np.sin(angle[i])]
+    y = [0, -par_map[d] * np.cos(angle[i])]
+
+    line.set_data(x, y)
+    time_text.set_text(time_template.format(i * interval_value))
+    return line, time_text
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(time)),
+                              interval=25, blit=True, init_func=init)
+
+if save_animation:
+    ani.save('pendulum_swing_up.mp4', writer='avconv', fps=15)
 
 plt.show()
