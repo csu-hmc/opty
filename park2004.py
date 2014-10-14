56b1828f6f69dfdad98f2a41fb743e98aeb613c0
Jason K. Moore <moorepants@gmail.com>
2014-10-13 22:48:50 -0400

    Added numerical parameters to park2004.py.

--- park2004.py
+++ park2004.py
@@ -2,9 +2,11 @@
 
 from collections import OrderedDict
 
+import numpy as np
 from sympy import symbols, trigsimp, Matrix, simplify, solve
 from sympy.physics.mechanics import dynamicsymbols, ReferenceFrame, Point
 from sympy.physics.mechanics import inertia, RigidBody, KanesMethod, msubs
+import yeadon
 
 sym_kwargs = {'positive': True, 'real': True}
 dynamicsymbols._t = symbols('t', **sym_kwargs)
@@ -15,35 +17,34 @@ class PlanarStandingHumanOnMovingPlatform():
     model representing a human standing on a antero-posteriorly moving
     platform similar to the one found in Park et. al 2004.
 
-    Variables
-    =========
+    Time Varying Parameters
+    =======================
     theta_a :
-        angle between the ankle and the legs, plantar flexion is positive
+        Angle of legs wrt to foot, plantar flexion is positive.
     theta_h :
-        angle between the legs and the hip, extension is positive
+        Angle of torso wrt to legs, extension is positive.
     omega_a :
-        angular rate of the legs wrt to the ankle
+        Angular rate of the legs wrt the foot.
     omega_h :
-        angular rate of the torso wrt to the legs
+        Angular rate of the torso wrt the legs.
     T_a :
-        torque applied between the foot and the leg, positive torque causes
-        plantar flexion
+        Torque applied between the foot and the legs, positive torque causes
+        plantar flexion.
     T_h :
-        torque applied between the leg and the torso, positive torque causes
-        extension
+        Torque applied between the legs and the torso, positive torque
+        causes extension.
     a :
         Antero-posterior acceleration of the platform and foot.
 
     Constant Parameters
     ===================
-
-    l_L : leg length
-    d_L : leg_com_length
-    d_T : torso_com_length
-    m_L : leg_mass
-    m_T : torso_mass
-    I_L : leg_inertia
-    I_T : torso_inertia
+    l_L : distance from ankle to hip
+    d_L : distance from ankle to legs center of mass
+    d_T : distance from hip to torso center of mass
+    m_L : mass of legs
+    m_T : mass of torso
+    I_L : moment of inertia of the legs
+    I_T : moment of inertia of the torso
     g : acceleration due to gravity
 
     Equations of Motion
@@ -78,8 +79,8 @@ class PlanarStandingHumanOnMovingPlatform():
 
     Additional constants
     --------------------
-    k_{T_a-theta_a}, k_{T_a-theta_h}, k_{T_a-omega_a}, k_{T_a-omega_h}
-    k_{T_h-theta_a}, k_{T_h-theta_h}, k_{T_h-omega_a}, k_{T_h-omega_h}
+    k = [k_00, k_01, k_02, k_03]
+        [k_10, k_11, k_12, k_{T_h-omega_h}
 
     x = [q, u]
     r = [a]
@@ -94,7 +95,7 @@ class PlanarStandingHumanOnMovingPlatform():
 
     """
 
-    def create_states(self):
+    def _create_states(self):
 
         theta_a, theta_h = dynamicsymbols('theta_a, theta_h')
         omega_a, omega_h = dynamicsymbols('omega_a, omega_h')
@@ -107,7 +108,9 @@ class PlanarStandingHumanOnMovingPlatform():
         self.speeds['omega_a'] = omega_a
         self.speeds['omega_h'] = omega_h
 
-    def create_specified(self):
+        self.time = dynamicsymbols._t
+
+    def _create_specified(self):
 
         self.specified = OrderedDict()
         self.specified['platform_position'] = dynamicsymbols('x')
@@ -116,32 +119,26 @@ class PlanarStandingHumanOnMovingPlatform():
         self.specified['ankle_torque'] = dynamicsymbols('T_a')
         self.specified['hip_torque'] = dynamicsymbols('T_h')
 
-    def create_parameters(self):
-
-        leg_length = symbols('l_L', **sym_kwargs)
-        leg_com_length, torso_com_length = symbols('d_L, d_T', **sym_kwargs)
-        leg_mass, torso_mass = symbols('m_L, m_T', **sym_kwargs)
-        leg_inertia, torso_inertia = symbols('I_Lz, I_Tz', **sym_kwargs)
-        g = symbols('g', **sym_kwargs)
+    def _create_parameters(self):
 
         self.parameters = OrderedDict()
-        self.parameters['leg_length'] = leg_length
-        self.parameters['leg_com_length'] = leg_com_length
-        self.parameters['torso_com_length'] = torso_com_length
-        self.parameters['leg_mass'] = leg_mass
-        self.parameters['torso_mass'] = torso_mass
-        self.parameters['leg_inertia'] = leg_inertia
-        self.parameters['torso_inertia'] = torso_inertia
-        self.parameters['g'] = g
+        self.parameters['leg_length'] = symbols('l_L', **sym_kwargs)
+        self.parameters['leg_com_length'] = symbols('d_L', **sym_kwargs)
+        self.parameters['torso_com_length'] = symbols('d_T', **sym_kwargs)
+        self.parameters['leg_mass'] = symbols('m_L', **sym_kwargs)
+        self.parameters['torso_mass'] = symbols('m_T', **sym_kwargs)
+        self.parameters['leg_inertia'] = symbols('I_L', **sym_kwargs)
+        self.parameters['torso_inertia'] = symbols('I_T', **sym_kwargs)
+        self.parameters['g'] = symbols('g', **sym_kwargs)
 
-    def create_reference_frames(self):
+    def _create_reference_frames(self):
 
         self.frames = OrderedDict()
         self.frames['inertial'] = ReferenceFrame('I')
         self.frames['leg'] = ReferenceFrame('L')
         self.frames['torso'] = ReferenceFrame('T')
 
-    def orient_reference_frames(self):
+    def _orient_reference_frames(self):
 
         self.frames['leg'].orient(self.frames['inertial'],
                                   'Axis',
@@ -153,7 +150,7 @@ class PlanarStandingHumanOnMovingPlatform():
                                     (self.coordinates['theta_h'],
                                      self.frames['leg'].z))
 
-    def create_points(self):
+    def _create_points(self):
 
         self.points = OrderedDict()
         self.points['origin'] = Point('O')
@@ -162,7 +159,7 @@ class PlanarStandingHumanOnMovingPlatform():
         self.points['leg_mass_center'] = Point('L_o')
         self.points['torso_mass_center'] = Point('T_o')
 
-    def set_positions(self):
+    def _set_positions(self):
 
         vec = self.specified['platform_position'] * self.frames['inertial'].x
         self.points['ankle'].set_pos(self.points['origin'], vec)
@@ -176,16 +173,14 @@ class PlanarStandingHumanOnMovingPlatform():
         vec = self.parameters['torso_com_length'] * self.frames['torso'].y
         self.points['torso_mass_center'].set_pos(self.points['hip'], vec)
 
-    def define_kin_diff_eqs(self):
-
-        time = dynamicsymbols._t
+    def _define_kin_diff_eqs(self):
 
         self.kin_diff_eqs = (self.speeds['omega_a'] -
-                             self.coordinates['theta_a'].diff(time),
+                             self.coordinates['theta_a'].diff(self.time),
                              self.speeds['omega_h'] -
-                             self.coordinates['theta_h'].diff(time))
+                             self.coordinates['theta_h'].diff(self.time))
 
-    def set_angular_velocities(self):
+    def _set_angular_velocities(self):
 
         vec = self.speeds['omega_a'] * self.frames['inertial'].z
         self.frames['leg'].set_ang_vel(self.frames['inertial'], vec)
@@ -193,7 +188,7 @@ class PlanarStandingHumanOnMovingPlatform():
         vec = self.speeds['omega_h'] * self.frames['leg'].z
         self.frames['torso'].set_ang_vel(self.frames['leg'], vec)
 
-    def set_linear_velocities(self):
+    def _set_linear_velocities(self):
 
         self.points['origin'].set_vel(self.frames['inertial'], 0)
 
@@ -213,7 +208,7 @@ class PlanarStandingHumanOnMovingPlatform():
                                                      self.frames['inertial'],
                                                      self.frames['torso'])
 
-    def create_inertia_dyadics(self):
+    def _create_inertia_dyadics(self):
 
         leg_inertia_dyadic = inertia(self.frames['leg'], 0, 0,
                                      self.parameters['leg_inertia'])
@@ -227,7 +222,7 @@ class PlanarStandingHumanOnMovingPlatform():
         self.central_inertias['torso'] = (torso_inertia_dyadic,
                                           self.points['torso_mass_center'])
 
-    def create_rigid_bodies(self):
+    def _create_rigid_bodies(self):
 
         self.rigid_bodies = OrderedDict()
 
@@ -243,7 +238,7 @@ class PlanarStandingHumanOnMovingPlatform():
                                                self.parameters['torso_mass'],
                                                self.central_inertias['torso'])
 
-    def create_loads(self):
+    def _create_loads(self):
 
         self.loads = OrderedDict()
 
@@ -263,22 +258,22 @@ class PlanarStandingHumanOnMovingPlatform():
                                       self.specified['hip_torque'] *
                                       self.frames['inertial'].z)
 
-    def setup_problem(self):
-        self.create_states()
-        self.create_specified()
-        self.create_parameters()
-        self.create_reference_frames()
-        self.orient_reference_frames()
-        self.create_points()
-        self.set_positions()
-        self.define_kin_diff_eqs()
-        self.set_angular_velocities()
-        self.set_linear_velocities()
-        self.create_inertia_dyadics()
-        self.create_rigid_bodies()
-        self.create_loads()
+    def _setup_problem(self):
+        self._create_states()
+        self._create_specified()
+        self._create_parameters()
+        self._create_reference_frames()
+        self._orient_reference_frames()
+        self._create_points()
+        self._set_positions()
+        self._define_kin_diff_eqs()
+        self._set_angular_velocities()
+        self._set_linear_velocities()
+        self._create_inertia_dyadics()
+        self._create_rigid_bodies()
+        self._create_loads()
 
-    def generate_eoms(self):
+    def _generate_eoms(self):
 
         self.kane = KanesMethod(self.frames['inertial'],
                                 self.coordinates.values(),
@@ -288,14 +283,14 @@ class PlanarStandingHumanOnMovingPlatform():
         fr, frstar = self.kane.kanes_equations(self.loads.values(),
                                                self.rigid_bodies.values())
 
-        sub = {self.specified['platform_speed'].diff(dynamicsymbols._t):
+        sub = {self.specified['platform_speed'].diff(self.time):
                self.specified['platform_acceleration']}
 
         self.fr_plus_frstar = trigsimp(fr + frstar).subs(sub)
 
-    def generate_rhs(self):
+    def _generate_rhs(self):
 
-        udots = Matrix([s.diff() for s in self.speeds.values()])
+        udots = Matrix([s.diff(self.time) for s in self.speeds.values()])
 
         m1 = self.fr_plus_frstar.diff(udots[0])
         m2 = self.fr_plus_frstar.diff(udots[1])
@@ -306,15 +301,11 @@ class PlanarStandingHumanOnMovingPlatform():
 
         udot = self.mass_matrix.LUsolve(self.forcing_vector)
         qdot_map = self.kane.kindiffdict()
-        qdot = Matrix([qdot_map[q.diff()] for q in self.coordinates.values()])
+        qdot = Matrix([qdot_map[q.diff(self.time)] for q in
+                       self.coordinates.values()])
         self.rhs = qdot.col_join(udot)
 
-    def derive(self):
-        self.setup_problem()
-        self.generate_eoms()
-        self.generate_rhs()
-
-    def create_symbolic_controller(self):
+    def _create_symbolic_controller(self):
 
         states = self.coordinates.values() + self.speeds.values()
         inputs = self.specified.values()[-2:]
@@ -337,9 +328,47 @@ class PlanarStandingHumanOnMovingPlatform():
 
         self.controller_dict = solve(T - K * (xeq - x), inputs)
 
-    def generate_closed_loop_eoms(self):
-
-        self.create_symbolic_controller()
+    def _generate_closed_loop_eoms(self):
 
         self.fr_plus_frstar_closed = msubs(self.fr_plus_frstar,
                                            self.controller_dict)
+
+    def _numerical_parameters(self):
+
+        h = yeadon.Human('JasonYeadonMeas.txt')
+
+        hip_pos = h.J1.pos
+        ankle_pos = h.J2.solids[1].pos
+        leg_length = np.sqrt(np.sum(np.asarray(ankle_pos - hip_pos)**2))
+
+        leg_mass, leg_com, leg_inertia = \
+            h.combine_inertia(['j0', 'j1', 'j2', 'j3', 'j4',
+                               'k0', 'k1', 'k2', 'k3', 'k4'])
+
+        leg_com_length = np.sqrt(np.sum(np.asarray(ankle_pos - leg_com)**2))
+
+        torso_mass, torso_com, torso_inertia = \
+            h.combine_inertia(['P', 'T', 'C', 'A1', 'A2', 'B1', 'B2'])
+
+        p = {}
+        p['leg_length'] = leg_length
+        p['leg_com_length'] = leg_com_length
+        p['leg_mass'] = leg_mass
+        p['leg_inertia'] = leg_inertia[0, 0]
+        p['torso_com_length'] = torso_com[2, 0]
+        p['torso_mass'] = torso_mass
+        p['torso_inertia'] = torso_inertia[0, 0]
+        p['g'] = 9.81
+
+        self.parameter_map = OrderedDict()
+
+        for k, v in self.parameters.items():
+            self.parameter_map[v] = p[k]
+
+    def derive(self):
+        self._setup_problem()
+        self._generate_eoms()
+        self._generate_rhs()
+        self._create_symbolic_controller()
+        self._generate_closed_loop_eoms()
+        self._numerical_parameters()
