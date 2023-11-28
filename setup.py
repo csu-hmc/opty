d7defd045c985a48f5d97c7ff783098bba24b591
Jason K. Moore <moorepants@gmail.com>
2023-11-28 15:07:03 +0100

    Cleanup of setup.py for v1.2.0.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-
 from setuptools import setup, find_packages
 
 exec(open('opty/version.py').read())
@@ -17,33 +15,39 @@ setup(
     description=('Tools for optimizing dynamic systems using direct '
                  'collocation.'),
     long_description=open('README.rst').read(),
-    install_requires=['numpy>=1.19.0',
-                      'scipy>=1.5.0',
-                      'sympy>=1.6.0',
-                      'cython>=0.29.19',
-                      'cyipopt>=1.1.0',
-                      ],
-    extras_require={'examples': ['pydy>=0.5.0',
-                                 'matplotlib>=3.2.0',
-                                 'tables',
-                                 'yeadon',
-                                 'pandas',
-                                 ],
-                    'doc': ['sphinx',
-                            'numpydoc',
-                            ],
-                    },
+    install_requires=[
+        'cyipopt>=1.1.0',
+        'cython>=0.29.19',
+        'numpy>=1.19.0',
+        'scipy>=1.5.0',
+        'sympy>=1.6.0',
+    ],
+    extras_require={
+        'examples': [
+            'matplotlib>=3.2.0',
+            'pandas',
+            'pydy>=0.5.0',
+            'tables',
+            'yeadon',
+        ],
+        'doc': [
+            'numpydoc',
+            'sphinx',
+        ],
+    },
     tests_require=['pytest'],
-    classifiers=['Programming Language :: Python',
-                 'Programming Language :: Python :: 3.8',
-                 'Programming Language :: Python :: 3.9',
-                 'Programming Language :: Python :: 3.10',
-                 'Programming Language :: Python :: 3.11',
-                 'Operating System :: OS Independent',
-                 'Development Status :: 4 - Beta',
-                 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: BSD License',
-                 'Natural Language :: English',
-                 'Topic :: Scientific/Engineering',
-                 'Topic :: Scientific/Engineering :: Physics']
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ]
 )
