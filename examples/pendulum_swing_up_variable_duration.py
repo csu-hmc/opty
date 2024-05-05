25a2087e097d22be013798ec855adcd988813cef
Jason K. Moore <moorepants@gmail.com>
2024-05-05 11:45:35 +0200

    Added variable duration pendulum swing up to the documentation.

--- examples/pendulum_swing_up_variable_duration.py
+++ examples/pendulum_swing_up_variable_duration.py
@@ -2,9 +2,12 @@
 
 http://hmc.csuohio.edu/resources/human-motion-seminar-jan-23-2014
 
-A simple pendulum is controlled by a torque at its joint. The goal is to
-swing the pendulum from its rest equilibrium to a target angle by minimizing
-the energy used to do so.
+A simple pendulum is controlled by a torque at its joint. The goal is to swing
+the pendulum from its rest equilibrium to a target angle by minimizing the
+energy used to do so in a minimal amount of time.
+
+Solves the same problem as in ``pendulum_swing_up.py`` but with a variable
+total duration.
 
 """
 
@@ -13,7 +16,7 @@ from collections import OrderedDict
 import numpy as np
 import sympy as sym
 from opty.direct_collocation import Problem
-from opty.utils import building_docs, create_objective_function
+from opty.utils import building_docs
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -21,22 +24,19 @@ target_angle = np.pi
 num_nodes = 500
 save_animation = False
 
-interval_value = sym.symbols('h')
-
 # Symbolic equations of motion
-I, m, g, d, t = sym.symbols('I, m, g, d, t')
+m, g, d, t, h = sym.symbols('m, g, d, t, h')
 theta, omega, T = sym.symbols('theta, omega, T', cls=sym.Function)
 
 state_symbols = (theta(t), omega(t))
-constant_symbols = (I, m, g, d)
+constant_symbols = (m, g, d)
 specified_symbols = (T(t),)
 
 eom = sym.Matrix([theta(t).diff() - omega(t),
-                  I * omega(t).diff() + m * g * d * sym.sin(theta(t)) - T(t)])
+                  m*d**2*omega(t).diff() + m*g*d*sym.sin(theta(t)) - T(t)])
 
 # Specify the known system parameters.
 par_map = OrderedDict()
-par_map[I] = 1.0
 par_map[m] = 1.0
 par_map[g] = 9.81
 par_map[d] = 1.0
@@ -46,7 +46,7 @@ par_map[d] = 1.0
 def obj(free):
     """Minimize the sum of the squares of the control torque."""
     T = free[2 * num_nodes:]
-    return free[-1] * np.sum(T**2)
+    return free[-1]*np.sum(T**2)
 
 
 def obj_grad(free):
@@ -57,23 +57,22 @@ def obj_grad(free):
     return grad
 
 
-# Specify the symbolic instance constraints, i.e. initial and end
-# conditions.
+# Specify the symbolic instance constraints, i.e. initial and end conditions
+# using node numbers 1 to N.
 instance_constraints = (theta(1),
                         theta(num_nodes) - target_angle,
                         omega(1),
                         omega(num_nodes))
 
 # Create an optimization problem.
-prob = Problem(obj, obj_grad, eom, state_symbols, num_nodes, interval_value,
+prob = Problem(obj, obj_grad, eom, state_symbols, num_nodes, h,
                known_parameter_map=par_map,
                instance_constraints=instance_constraints,
-               bounds={T(t): (-2.0, 2.0), interval_value: (0.0, 0.5)})
+               time_symbol=t,
+               bounds={T(t): (-2.0, 2.0), h: (0.0, 0.5)})
 
-# Use a random positive initial guess.
-initial_guess = np.random.randn(prob.num_free)
+# Use a zero as an initial guess.
 initial_guess = np.zeros(prob.num_free)
-initial_guess[-1] = 0.01
 
 # Find the optimal solution.
 solution, info = prob.solve(initial_guess)
@@ -85,7 +84,6 @@ prob.plot_objective_value()
 
 # Display animation
 if not building_docs():
-    #time = np.linspace(0.0, duration, num=num_nodes)
     interval_value = solution[-1]
     time = np.linspace(0.0, num_nodes*solution[-1], num=num_nodes)
     angle = solution[:num_nodes]
@@ -109,14 +107,14 @@ if not building_docs():
         y = [0, -par_map[d] * np.cos(angle[i])]
 
         line.set_data(x, y)
-        time_text.set_text(time_template.format(i * interval_value))
+        time_text.set_text(time_template.format(i*interval_value))
         return line, time_text
 
     ani = animation.FuncAnimation(fig, animate, np.arange(1, len(time)),
                                   interval=25, blit=True, init_func=init)
 
     if save_animation:
-        ani.save('pendulum_swing_up.mp4', writer='ffmpeg',
+        ani.save('pendulum_swing_up_variable_duration.mp4', writer='ffmpeg',
                  fps=1 / interval_value)
 
 plt.show()
