7fff4279469ae5c48241bba9f57ddc575184b1d0
Jason K. Moore <moorepants@gmail.com>
2024-05-20 10:43:46 +0200

    Corrected variant > version.

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Operating System :: OS Independent',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
