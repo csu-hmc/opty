b038b49bc785f89fd361130b479f930db5aacced
Peter Stahlecker <peter.stahlecker@gmail.com>
2024-07-30 16:49:06 +0200

    4th commit, hopefully nor screwed up. Corrected everything except fps = 30, I am unsure what to do

--- examples-gallery/plot_crane_moving_a_load.py
+++ examples-gallery/plot_crane_moving_a_load.py
@@ -1,11 +1,11 @@
  # %%
 """
-Crane moving a load
+Crane Moving a Load
 ===================
 
 A load is moved by a crane. The load is rotably connected to the crane by a
 massless rod. The goal is to move the load from the initial position to the
-final position in the shortest possible time. The load must not 'overswing'
+final position in the shortest possible time. The load must not over-swing
 the final position. The load is moved by a force applied to its
 suspension point. The jib of the crane is extended in the horizontal
 X direction.
@@ -20,12 +20,8 @@ X direction.
 **States**
 
 - xc: x-coordinate of the mover [m]
-- xl: x-coordinate of the load [m]  dependent coordinate
-- yl: y-coordinate of the load [m]  dependent coordinate
 - q: angle of the rod [rad]
 - uxc: velocity of the mover in x-direction [m/s]
-- uxl: velocity of the load in x-direction [m/s] dependent speed
-- uyl: velocity of the load in y-direction [m/s] dependent speed
 - u: angular velocity of the rod [rad/s]
 
 **Specifieds**
@@ -34,7 +30,6 @@ X direction.
 
 """
 import sympy.physics.mechanics as me
-from collections import OrderedDict
 import numpy as np
 import sympy as sm
 from scipy.interpolate import CubicSpline
@@ -46,7 +41,6 @@ from matplotlib import patches
 
 # %%
 # Set up Kane's equations of motion.
-# Just the standard way of doing it.
 
 N, A = sm.symbols('N A', cls = me.ReferenceFrame)
 t = me.dynamicsymbols._t
@@ -68,9 +62,9 @@ P2.v2pt_theory(P1, N, A)
 P1a = me.Particle('P1a', P1, m1)
 P2a = me.Particle('P2a', P2, m2)
 
-BODY = [P1a, P2a]
+bodies = [P1a, P2a]
 
-FL = [(P1, F * N.x - m1*g*N.y), (P2, -m2*g*N.y)]
+forces = [(P1, F * N.x - m1*g*N.y), (P2, -m2*g*N.y)]
 kd = sm.Matrix([uxc - xc.diff(t), u - q.diff(t), uxl - xl.diff(t),
     uyl - yl.diff(t)])
 
@@ -91,29 +85,26 @@ KM = me.KanesMethod(N,
     configuration_constraints=config_constr,
     velocity_constraints=speed_constr)
 
-(fr, frstar) = KM.kanes_equations(BODY, FL)
-EOM = kd.col_join(fr + frstar)
-EOM = EOM.col_join(config_constr)
-sm.pprint(EOM)
+fr, frstar = KM.kanes_equations(bodies, forces)
+eom = kd.col_join(fr + frstar)
+eom = eom.col_join(config_constr)
+sm.pprint(eom)
 # %%
 # Set up the optimization problem and solve it.
-# Note: While opty could calculate the reaction forces on, say, the mover,
-# (add them to the state variables), my trials showed that even with this
-# small example the calculation time increases enourmously.
 
 state_symbols = tuple((*q_ind, *q_dep, *u_ind, *u_dep))
 constant_symbols = (l, m1, m2, g)
 specified_symbols = (F,)
-h_opty = sm.symbols('h_opty')
-unknown_symbols = []
+h = sm.symbols('h')
 methode = "backward euler"
 
 num_nodes = 250
-duration =(num_nodes - 1) * h_opty
-interval_value = h_opty
+duration =(num_nodes - 1) * h
+interval_value = h
 
+# %%
 # Specify the known system parameters.
-par_map = OrderedDict()
+par_map = {}
 par_map[l] = 5.0
 par_map[m1] = 1.0
 par_map[m2] = 10.0
@@ -121,8 +112,10 @@ par_map[g] = 9.81
 
 t0, tf = 0.0, duration
 
+# %%
+# Set up the objective function and its gradient.
 def obj(free):
-    """Minimize h_opty, the time interval between nodes."""
+    """Minimize h, the time interval between nodes."""
     return free[-1]
 
 def obj_grad(free):
@@ -130,15 +123,18 @@ def obj_grad(free):
     grad[-1] = 1.
     return grad
 
-# start location and and final location of the load.
-anfang = 0.0
-ende = 15.0
+# %%
+# Starting and final location of the load.
+starting_location = 0.0
+ending_location = 15.0
 
 # %%
 # Set the initial and final states to form the instance constraints.
-initial_state_constraints = {xc: anfang, xl: anfang, yl: -par_map[l], q: 0.0,
+initial_state_constraints = {xc: starting_location, xl: starting_location,
+    yl: -par_map[l], q: 0.0,
     uxc: 0.0, uxl: 0.0, uyl: 0.0, u: 0.0}
-final_state_constraints = {xc: ende, xl: ende, yl: -par_map[l], q: 0.0,
+final_state_constraints = {xc: ending_location, xl: ending_location,
+    yl: -par_map[l], q: 0.0,
     uxc: 0.0, uxl: 0.0, uyl: 0.0, u: 0.0}
 instance_constraints = (
 ) + tuple(
@@ -149,22 +145,23 @@ instance_constraints = (
         final_state_constraints.items()
 )
 # %%
-# Forcing h_opty > 0.0 sometimes avoids negative 'solutions'. Here it also
+# Forcing h > 0.0 sometimes avoids negative 'solutions'. Here it also
 # seem to help with the convergence of the optimization: Different bounds,
-# e,g, h_opty = (1.e-4, 1.) gives unreasonable results.
+# e,g, h in (1.e-4, 1.) gives unreasonable results.
 
 bounds = {
     F: (-20., 20.),
-    xl: (anfang, ende),
-    xc: (anfang, ende),
-    h_opty: (1.e-5, 0.5),
+    xl: (starting_location, ending_location),
+    xc: (starting_location, ending_location),
+    h: (1.e-5, 0.5),
     }
 
+# %%
 # Create the optimization problem.
 prob = Problem(
     obj,
     obj_grad,
-    EOM,
+    eom,
     state_symbols,
     num_nodes,
     interval_value,
@@ -174,9 +171,9 @@ prob = Problem(
     integration_method=methode,
     )
 # %%
-# Using a 'reasonable' initial guess speeds up the optimization process a lot.
-# 'Bad' initial guesses may even prevent convergence.
-i1 = [(ende - anfang) / num_nodes * i for i in range(num_nodes)]
+# Set up the initial guess and plot it.
+i1 = [(ending_location - starting_location) / num_nodes * i
+    for i in range(num_nodes)]
 i2 = [0. for _ in range(num_nodes)]
 i3 = i1
 i4 = [-par_map[l] for _ in range(num_nodes)]
@@ -184,25 +181,25 @@ i5 = [0. for _ in range(5*num_nodes)]
 i6 = [0.01]
 initial_guess = np.array(i1 + i2 + i3 + i4 + i5 + i6)
 
-# %%
-# Plot the initial guess.
 prob.plot_trajectories(initial_guess)
 
-
-# this allows to change the maximum number of iterations.
+# %%
+# This allows to change the maximum number of iterations.
 # Standard is 3000.
 prob.add_option('max_iter', 1000)
+
+# %%
 # Find the optimal solution.
-for _ in range(1):
-    solution, info = prob.solve(initial_guess)
-    print('message from optimizer:', info['status_msg'])
-    prob.plot_objective_value()
-    initial_guess = solution
+
+solution, info = prob.solve(initial_guess)
+print('message from optimizer:', info['status_msg'])
+prob.plot_objective_value()
+initial_guess = solution
 print('Iterations needed', len(prob.obj_value))
 print(f"Objective value {solution[-1]: .3e}")
 
 # %%
-# plot the accuracy of the solution.
+# Plot the accuracy of the solution.
 prob.plot_constraint_violations(solution)
 
 # %%
@@ -228,11 +225,10 @@ pL, pL_vals = zip(*par_map.items())
 coords_lam = sm.lambdify((*state_symbols, F, *pL),
     coordinates, cse=True)
 
-# Dimensions of the mover and the load.
 width, height, radius = 0.5, 0.5, 0.5
 
 def init_plot():
-    xmin, xmax = anfang-1, ende+1
+    xmin, xmax = starting_location-1, ending_location+1
     ymin, ymax = -par_map[l]-1, 1
 
     fig, ax = plt.subplots(figsize=(8, 8))
@@ -243,12 +239,12 @@ def init_plot():
     ax.set_ylabel('Y-axis [m]')
 
     ax.axhline(0., color='black', lw=1.5)
-    ax.axvline(ende+radius, color='black', lw=1.0)
-    ax.axvline(anfang-radius, color='black', lw=1.0)
-    ax.annotate(f'walls', xy=(ende+radius, -2),
+    ax.axvline(ending_location+radius, color='black', lw=1.0)
+    ax.axvline(starting_location-radius, color='black', lw=1.0)
+    ax.annotate(f'walls', xy=(ending_location+radius, -2),
         arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-.2',
         lw=0.25), xytext=(8, 0.5), fontsize=9)
-    ax.annotate(f'', xy=(anfang-radius, -1),
+    ax.annotate(f'', xy=(starting_location-radius, -1),
         arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=-.2',
         lw=0.25), xytext=(8, 0.5))
 
@@ -265,7 +261,6 @@ def init_plot():
 
 fig, ax, line1, recht, load, pfeil = init_plot()
 
-# Function to update the plot for each animation frame
 def update(t):
     message = f'running time {t:.2f} sec \n The red arrow shows the force.'
     ax.set_title(message, fontsize=12)
@@ -279,16 +274,12 @@ def update(t):
     pfeil.set_UVC(input_sol(t) , 0.25)
     return line1, recht, load, pfeil
 
-
-# Create the animation
 anim = animation.FuncAnimation(fig, update, frames=np.arange(t0, tf, 1 / fps),
     interval=fps)
 
 # %%
 # A frame from the animation.
 fig, ax, line1, recht, load, pfeil = init_plot()
-
 # sphinx_gallery_thumbnail_number = 6
 update(2.0)
-
 plt.show()
