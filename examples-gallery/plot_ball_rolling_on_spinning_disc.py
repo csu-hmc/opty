621a2d5f03295c18ede2aa265bf81ac707b28c54
Peter Stahlecker <peter.stahlecker@gmail.com>
2024-09-17 08:26:00 +0200

    changes as suggested

--- examples-gallery/plot_ball_rolling_on_spinning_disc.py
+++ examples-gallery/plot_ball_rolling_on_spinning_disc.py
@@ -349,19 +349,19 @@ prob = Problem(
 )
 
 # %%
-# The initial guess should meet the configuration constrains. Those below were
-# chosen to get a better initial guess, now stored in
+# The initial guess should meet the configuration constrains. Those below are
+# used to get a better initial guess, stored in
 # 'ball_rolling_on_spinning_disc_solution.npy'.
 
-# -i1b = np.zeros(num_nodes)
-# -i2 = np.linspace(initial_state_constraints[x],
-#    final_state_constraints[x], num_nodes)
-# -i1a = i2 / par_map[r]
-# -i3 = np.linspace(initial_state_constraints[y],
-#    final_state_constraints[y], num_nodes)
-# -i1 = -i3 / par_map[r]
-# -i4 = np.zeros(8*num_nodes)
-# -initial_guess = np.hstack((i1,i1a, i1b, i2, i3, i4, 0.01))
+i1b = np.zeros(num_nodes)
+i2 = np.linspace(initial_state_constraints[x],
+    final_state_constraints[x], num_nodes)
+i1a = i2 / par_map[r]
+i3 = np.linspace(initial_state_constraints[y],
+    final_state_constraints[y], num_nodes)
+i1 = -i3 / par_map[r]
+i4 = np.zeros(8*num_nodes)
+initial_guess = np.hstack((i1,i1a, i1b, i2, i3, i4, 0.01))
 
 # %%
 # This way the maximum number of interations may be changed.
@@ -375,10 +375,13 @@ solution, info = prob.solve(initial_guess)
 print('message from optimizer:', info['status_msg'])
 print('Iterations needed', len(prob.obj_value))
 print(f'Optimal h = {solution[-1]:.3e} sec')
-np.save('ball_rolling_on_spinning_disc_solution.npy', solution)
 
 # %%
-# PLot the objective value.
+# This way the solution may be saved for later use.
+# ```np.save('ball_rolling_on_spinning_disc_solution.npy', solution)```
+
+# %%
+# Plot the objective value.
 prob.plot_objective_value()
 
 # %%
