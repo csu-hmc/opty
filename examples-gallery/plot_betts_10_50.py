3aa45b0a60509cd7e245a3e525a70079d859194d
Jason K. Moore <moorepants@gmail.com>
2025-02-06 07:11:56 +0100

    Minor cleanup to betts 10 50.

--- examples-gallery/plot_betts_10_50.py
+++ examples-gallery/plot_betts_10_50.py
@@ -1,15 +1,13 @@
-"""
+r"""
 Delay Equation (Göllmann, Kern, and Maurer)
 ===========================================
 
-This is example 10.50 from Betts' book "Practical Methods for Optimal Control
-Using NonlinearProgramming", 3rd edition, Chapter 10: Test Problems.
+This is example 10.50 from [Betts2010]_.
 
-There are inequalities: :math:`x_i(t) + u_i(t) \\geq 0.3`.
-To handle them, I define additional  state variables :math:`q_i(t)` and use
-additional EOMs:
-
-:math:`q_i(t) = x_i(t) + u_i(t)`, and then use bounds on :math:`q_i(t)`.
+There are inequalities: :math:`x_i(t) + u_i(t) \geq 0.3`. To handle them,
+additional state variables :math:`q_i(t)` and additional equations of motion
+can be added: :math:`q_i(t) = x_i(t) + u_i(t)`, and then use bounds on
+:math:`q_i(t)`.
 
 **States**
 
@@ -20,17 +18,12 @@ additional EOMs:
 
 - :math:`u_1, u_2, u_3, u_4, u_5, u_6` : control variables
 
-Note: I simply copied the equations of motion, the bounds and the constants
-from the book. I do not know their meaning.
-
 """
-
 import numpy as np
 import sympy as sm
 import sympy.physics.mechanics as me
 from opty.direct_collocation import Problem
 from opty.utils import create_objective_function
-import matplotlib.pyplot as plt
 
 # %%
 # Equations of Motion
@@ -41,7 +34,6 @@ x1, x2, x3, x4, x5, x6 = me.dynamicsymbols('x1, x2, x3, x4, x5, x6')
 q1, q2, q3, q4, q5, q6 = me.dynamicsymbols('q1, q2, q3, q4, q5, q6')
 u1, u2, u3, u4, u5, u6 = me.dynamicsymbols('u1, u2, u3, u4, u5, u6')
 
-#Parameters
 x0 = 1.0
 u_minus_1, u0 = 0.0, 0.0
 
@@ -66,15 +58,16 @@ sm.pprint(eom)
 # -----------------------------------------
 num_nodes = 501
 t0, tf = 0.0, 1.0
-interval_value = (tf - t0) / (num_nodes - 1)
+interval_value = (tf - t0)/(num_nodes - 1)
 
 state_symbols = (x1, x2, x3, x4, x5, x6, q1, q2, q3, q4, q5, q6)
 unkonwn_input_trajectories = (u1, u2, u3, u4, u5, u6)
 
 # %%
 # Specify the objective function and form the gradient.
-objective = sm.Integral(x1**2 + x2**2 + x3**2 + x4**2 + x5**2 + x6**2 + u1**2
-        + u2**2 + u3**2 + u4**2 + u5**2 + u6**2, t)
+objective = sm.Integral(
+    x1**2 + x2**2 + x3**2 + x4**2 + x5**2 + x6**2 +
+    u1**2 + u2**2 + u3**2 + u4**2 + u5**2 + u6**2, t)
 
 obj, obj_grad = create_objective_function(
     objective,
@@ -82,7 +75,8 @@ obj, obj_grad = create_objective_function(
     unkonwn_input_trajectories,
     tuple(),
     num_nodes,
-    interval_value
+    interval_value,
+    time_symbol=t,
 )
 
 # %%
@@ -90,20 +84,18 @@ obj, obj_grad = create_objective_function(
 
 instance_constraints = (
     x1.func(t0) - 1.0,
-
     x2.func(t0) - x1.func(tf),
     x3.func(t0) - x2.func(tf),
     x4.func(t0) - x3.func(tf),
     x5.func(t0) - x4.func(tf),
     x6.func(t0) - x5.func(tf),
-
     q1.func(t0) - 0.5,
     q2.func(t0) - 0.5,
     q3.func(t0) - 0.5,
     q4.func(t0) - 0.5,
     q5.func(t0) - 0.5,
     q6.func(t0) - 0.5,
-    )
+)
 
 limit_value = np.inf
 bounds = {
@@ -119,28 +111,29 @@ bounds = {
 # Solve the Optimization Problem
 # ------------------------------
 
-prob = Problem(obj,
+prob = Problem(
+    obj,
     obj_grad,
     eom,
     state_symbols,
     num_nodes,
     interval_value,
-    instance_constraints= instance_constraints,
+    instance_constraints=instance_constraints,
     bounds=bounds,
+    time_symbol=t,
 )
 
-# Some initial guess
-initial_guess = np.random.rand(18*num_nodes) * 0.1
 prob.add_option('max_iter', 1000)
 
-# Find the optimal solution.
+initial_guess = np.random.rand(18*num_nodes) * 0.1
+
 for _ in range(1):
     solution, info = prob.solve(initial_guess)
     initial_guess = solution
     print(info['status_msg'])
-    print(f'Objective value achieved: {info['obj_val']:.4f}, as per the book '+
-        f'it is {3.10812211}, so the error is: '
-        f'{(info['obj_val'] - 3.10812211)/3.10812211*100:.3f} % ')
+    print(f'Objective value achieved: {info['obj_val']:.4f}, as per the book '
+          f'it is {3.10812211}, so the error is: '
+          f'{(info['obj_val'] - 3.10812211)/3.10812211*100:.3f} % ')
     print('\n')
 
 # %%
@@ -157,11 +150,13 @@ prob.plot_objective_value()
 
 # %%
 # Are the inequality constraints satisfied?
-min_q = np.min(solution[7*num_nodes: 12*num_nodes-1])
+min_q = np.min(solution[7*num_nodes:12*num_nodes-1])
 if min_q >= 0.3:
-    print(f"Minimal value of the q\u1D62 is: {min_q:.12f} >= 0.3, so satisfied")
+    print(f"Minimal value of the q\u1D62 is: {min_q:.12f} >= 0.3, "
+          f"so satisfied.")
 else:
-    print(f"Minimal value of the q\u1D62 is: {min_q:.12f} < 0.3, so not satisfied")
+    print(f"Minimal value of the q\u1D62 is: {min_q:.12f} < 0.3, "
+          f"so not satisfied.")
 
 # %%
 # sphinx_gallery_thumbnail_number = 2
