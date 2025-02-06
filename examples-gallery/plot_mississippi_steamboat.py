24e244d852484df4af2a0eb81b14f07a1720dcd2
Peter Stahlecker <peter.stahlecker@gmail.com>
2025-02-06 16:27:23 +0100

    changed 7 / we wordings

--- examples-gallery/plot_mississippi_steamboat.py
+++ examples-gallery/plot_mississippi_steamboat.py
@@ -1,4 +1,3 @@
-# %%
 """
 Mississippi Steamboat
 =====================
@@ -12,6 +11,8 @@ rigid bodies.
 By running the wheels at different speeds, the boat can be steered.
 The water speed is assumed to be zero. Gravity, in the negative Z direction, is
 unimportant here, therefore disregarded.
+
+Of *interest* here may be the equations of motion.
 
 **Constants**
 
@@ -55,8 +56,8 @@ from matplotlib.patches import Rectangle
 
 
 # %%
-# Set up the Equations of Motion.
-# -------------------------------
+# Set up the Equations of Motion
+# ------------------------------
 #
 # Set up the geometry of the system.
 #
@@ -119,22 +120,22 @@ FPRW.set_vel(N, AoRW.vel(N) + uRW*AS.x.cross(-rW*N.z))
 # https://courses.lumenlearning.com/suny-physics/chapter/5-2-drag-forces/
 #
 #
-# I will lump :math:`\dfrac{1}{2} \rho C_D` into a single constant :math:`c`.
-# (In the code below, I will use :math:`c_S` for the steamboat and :math:`c_W`
-# for the wheels.)
+# :math:`\dfrac{1}{2} \rho C_D` are lumped into a single constant :math:`c`.
+# (In the code below, :math:`c_S` for the steamboat and :math:`c_W`
+# for the wheels will be used.)
 # In order to avoid numerical issues with .normalize(), sm.sqrt(..)
-# I will use the following:
+# the following will be used:
 #
-# :math:`F_{D_x} = -c A (\hat{A}.x \cdot \bar v)^2 \cdot \operatorname{sgn}(\hat{A}.x \cdot \bar v) \hat{A}.x`
-# :math:`F_{D_y} = -c A (\hat{A}.y \cdot \bar v)^2 \cdot \operatorname{sgn}(\hat{A}.y \cdot \bar v) \hat{A}.y`
+# :math:`F_{D_x} = -c A (\hat{A}.x \cdot \bar v)^2 \cdot \operatorname{sgn}
+# (\hat{A}.x \cdot \bar v) \hat{A}.x`
+# :math:`F_{D_y} = -c A (\hat{A}.y \cdot \bar v)^2 \cdot \operatorname{sgn}
+# (\hat{A}.y \cdot \bar v) \hat{A}.y`
 #
 # As an (infinitely often) differentiable approximation of the sign function,
-# I will use the fairly standard approximation:
+# the fairly standard approximation will be:
 #
 # :math:`\operatorname{sgn}(x) \approx \tanh( \alpha \cdot x )` with :math:`\alpha \gg 1`
 #
-
-
 
 helpx = AoS.vel(N).dot(AS.x)
 helpy = AoS.vel(N).dot(AS.y)
@@ -167,7 +168,7 @@ Tdrag = -cS*aS * u*AS.z.cross(AS.y)
 forces.append((AS, Tdrag))
 # %%
 # If :math:`u \neq 0`, the boat will rotate and a drag torque will act on it.
-# Let's look at the situation from the center of the boat to its bow. At a
+# Looking at the situation from the center of the boat to its bow: At a
 # distance :math:`r` from the center, The speed of a point a r is :math:`u \cdot r`.
 # The area is :math:`dr`, hence the force is :math:`-c_S (u \cdot r)^2 dr`. The
 # leverage at this point is :math:`r`, hence the torque is
@@ -184,7 +185,7 @@ tB = -cS*u**2*(aS**4 + bS**4)/32 * sm.tanh(20*u) * N.z
 forces.append((AS, tB))
 
 # %%
-# Set up the rigid bodies
+# Set up the rigid bodies.
 
 iXX = 0.5*mW*rW**2
 iYY = 0.25*mW*rW**2
@@ -216,8 +217,8 @@ fr, frstar = KM.kanes_equations(bodies, forces)
 eom = kd.col_join(fr + frstar)
 
 # %%
-# Set up the Optimization Problem and Solve it.
-# ---------------------------------------------
+# Set up the Optimization Problem and Solve It
+# --------------------------------------------
 #
 
 state_symbols = [q, x, y, qLW, qRW, u, ux, uy, uLW, uRW]
@@ -242,7 +243,8 @@ par_map[cW] = 0.75
 # Set up the objective function and its gradient. The objective function
 # to be minimized is:
 #
-# :math:`\text{obj} = \int_{t_0}^{t_f} \left( t_{LW}^2 + t_{RW}^2 \right) \, dt + \text{weight} \cdot h`
+# :math:`\text{obj} = \int_{t_0}^{t_f} \left( t_{LW}^2 + t_{RW}^2 \right) \, dt
+# + \text{weight} \cdot h`
 #
 # where weight > 0 is the relative importance of the duration of the motion,
 # and h > 0.
@@ -351,12 +353,11 @@ prob.plot_constraint_violations(solution)
 
 # %%
 # Plot the trajectories of the solution.
-fig, ax = plt.subplots(12, 1, figsize=(8, 20), constrained_layout=True,
-    sharex=True)
-prob.plot_trajectories(solution, ax)
+prob.plot_trajectories(solution)
 
 # %%
-# Animate the solutions and plot the results.
+# Animate the Solution
+# --------------------
 fps = 30
 
 def add_point_to_data(line, x, y):
