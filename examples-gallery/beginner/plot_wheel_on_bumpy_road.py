4246c956cb85d6637979b652d9fbe9248ee2f470
Peter Stahlecker <peter.stahlecker@gmail.com>
2025-03-13 14:03:15 +0100

    quarter car model, dampening c free parameter, names more meaningful.

--- examples-gallery/beginner/plot_wheel_on_bumpy_road.py
+++ examples-gallery/beginner/plot_wheel_on_bumpy_road.py
@@ -1,6 +1,6 @@
 r"""
-One Wheeler on Bumpy Road
-==========================
+Quarter Car Model on a Bumpy Road
+=================================
 
 Objective
 ---------
@@ -14,9 +14,9 @@ Objective
 Introduction
 ------------
 
-A one wheeler must move from a starting point to a final point on a bumpy road.
-The body is connected to the wheel on the road by a linear spring and an
-adjustable damper. These dampers seem available on luxury cars today.
+A quarter car model must move from a starting point to a final point on a bumpy
+road. The body is connected to the wheel on the road by a linear spring and a
+damper.
 
 https://www.tevema.com/what-is-the-best-suspension-for-luxury-cars/
 
@@ -30,21 +30,21 @@ points in the negative Z direction.
 - :math:`z_{car}` : z position of the car [m]
 - :math:`ux_{car}` : x velocity of the car [m/s]
 - :math:`uz_{car}` : z velocity of the car [m/s]
-- :math:`help_1` : to ensure the wheel always has a negative load, meaning it
-  will not jump [N]
-- :math:`help_2` : to ensure that the vertical motions of the body are not too
-  large [m]
-- :math:`help_3` : holds the acceleration of the body, which is to be minimized
-  [m/s^2]
-- :math:`help_4` : holds the accelerations of the wheel, only needed for the
-  animation. [m/s^2]
+- :math:`prevent_{jump}` : to ensure the wheel always has a negative load,
+  meaning it will not jump [N]
+- :math:`steady_{body}` : to ensure that the vertical motions of the body are
+  not too large [m]
+- :math:`accel_{body}` : holds the acceleration of the body, which is to be
+  minimized [m/s^2]
+- :math:`accel_{wheel}` : holds the accelerations of the wheel, only needed for
+  the animation. [m/s^2]
 
 
 
 **Fixed Parameters**
 
-- :math:`m_{car}` : mass of the car [kg]
-- :math:`m_{wheel}` : mass of the wheel [kg]
+- :math:`m_{car}` : sprung mass [kg]
+- :math:`m_{wheel}` : unsprung mass [kg]
 - :math:`g` : gravity [m/s^2]
 - :math:`l_0` : equilibrium length of the spring [m]
 - :math:`r_1, r_2, r_3, r_4, r_5` : parameters of the street.
@@ -52,11 +52,11 @@ points in the negative Z direction.
 
 **Free Parameters**
 
+- :math:`c` : damping constant [Ns/m]
 - :math:`k` : spring constant [N/m]
 
 **Unknown Trajectories**
 
-- :math:`c` : damping constant [Ns/m]
 - :math:`f_x` : driving force [N]
 
 """
@@ -69,7 +69,6 @@ from opty.utils import parse_free
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
 # %%
 # Set up the Equations of Motion
 #-------------------------------
@@ -79,20 +78,20 @@ O.set_vel(N, 0)
 t = me.dynamicsymbols._t
 x_car, z_car = me.dynamicsymbols('x_car z_car')
 ux_car, uz_car = me.dynamicsymbols('ux_car uz_car')
-c, help1, help2 = me.dynamicsymbols('c, help1, help2')
-help3, help4 = me.dynamicsymbols('help3 help4')
+prevent_jump, steady_body = me.dynamicsymbols('prevent_jump, steady_body')
+accel_body, accel_wheel = me.dynamicsymbols('accel_body accel_wheel')
 
 fx = me.dynamicsymbols('fx')
 m_car, m_wheel, g = sm.symbols('m_car m_wheel g')
 r1, r2, r3, r4, r5 = sm.symbols('r1 r2 r3 r4 r5')
 
-l_0, k = sm.symbols('  l_0, k')
+l_0, k, c = sm.symbols('  l_0, k, c')
 # %%
 # Define the rough surface of the street.
 
 def rough_surface(x_car):
     omega = 0.75
-    return sm.S(0.1) *  (r1*sm.sin(omega*x_car)**2 + r2*sm.sin(2*omega*x_car)**2
+    return sm.S(0.135 ) *  (r1*sm.sin(omega*x_car)**2 + r2*sm.sin(2*omega*x_car)**2
         + r3*sm.sin(3*omega*x_car)**2 + r4*sm.sin(7*omega*x_car)**2
         + r5*sm.sin(9*omega*x_car)**2)
 
@@ -136,17 +135,18 @@ eom = kd.col_join(fr + frstar)
 aux_1 = (rough_surface(x_car).diff(t)).subs({x_car.diff(t): ux_car})
 aux_1 = aux_1.diff(t)
 eom = eom.col_join(sm.Matrix([
-                                help1 - F_contact,
-                                help2 - (z_car - rough_surface(x_car)),
-                                help3 - (uz_car.diff(t)),
-                                help4 - aux_1
+                                prevent_jump - F_contact,
+                                steady_body - (z_car - rough_surface(x_car)),
+                                accel_body - (uz_car.diff(t)),
+                                accel_wheel - aux_1
 ]))
 
 print(f'eoms contains {sm.count_ops(eom)} equations and have shape {eom.shape}')
 # %%
 # Set up the Optimization Problem
 #--------------------------------
-state_symbols = [x_car, z_car, ux_car, uz_car, help1, help2, help3, help4]
+state_symbols = [x_car, z_car, ux_car, uz_car, prevent_jump, steady_body,
+                 accel_body, accel_wheel]
 
 h = sm.symbols('h')
 num_nodes = 301
@@ -197,8 +197,8 @@ bounds = {
     h: (0.0, 1.0),
     x_car: (0.0, 10.0),
     ux_car: (0.0, np.inf),
-    help1: (-1000.0, 0.0),
-    help2: (1.4, 1.7),
+    prevent_jump: (-1000.0, 0.0),
+    steady_body: (1.4, 1.7),
     c: (0.0, 750),
     k: (1000, 15000),
     fx: (-500, 500),
@@ -206,12 +206,12 @@ bounds = {
 
 # %%
 # Use an existing solution if available, else iterate to find one.
-fname =f'wheel_on_bumpy_road_{num_nodes}_nodes_solution.csv'
+fname =f'quarter_car_on_bumpy_road_{num_nodes}_nodes_solution.csv'
 aaa = 10
 if os.path.exists(fname):
     # use the existing solution
-    par_map[r3] = 0.1 + 0.05*4
-    par_map[r4] = 0.05*4
+    par_map[r3] = 0.1 + 0.075*4
+    par_map[r4] = 0.075*4
 
     prob = Problem(obj,
                obj_grad,
@@ -231,8 +231,8 @@ else:
     # Iterate to find the solution. As the convergence is not easy, one has to
     # start with a smooth road and then increase the roughness gradually.
     for i in range(5):
-        par_map[r3] = 0.1 + 0.05*i
-        par_map[r4] = 0.05*i
+        par_map[r3] = 0.1 + 0.075*i
+        par_map[r4] = 0.075*i
 
         prob = Problem(obj,
                obj_grad,
@@ -259,11 +259,14 @@ else:
 
 print('Sequence of unknown trajectories',
                prob.collocator.unknown_input_trajectories)
+#np.savetxt(fname, solution, fmt='%.12f')
 # %%
-_ = prob.plot_trajectories(solution)
+fig, axes = plt.subplots(9, 1, figsize=(7, 16), layout='tight')
+_ = prob.plot_trajectories(solution, axes=axes)
 # %%
 _ = prob.plot_constraint_violations(solution)
 print(f'value of optimum spring constant: {solution[-2]:.2f}')
+print(f'value of optimum damping constant: {solution[-3]:.2f}')
 
 # %%
 # Plot the road.
@@ -277,18 +280,17 @@ ax.plot(XX, rough_surface_lam(XX, r11, r22, r33, r44, r55))
 ax.set_xlabel('[m]')
 ax.set_ylabel('[m]')
 _ = ax.set_title('Road Profile')
-
 # %%
 # Animate the Solution
 # --------------------
-fps = 25
+fps = 100
 
 def add_point_to_data(line, x, y):
     # to trace the path of the point.
     old_x, old_y = line.get_data()
     line.set_data(np.append(old_x, x), np.append(old_y, y))
 
-state_vals, input_vals, _, _ = parse_free(solution, 8, 2, num_nodes,
+state_vals, input_vals, _, _ = parse_free(solution, 8, 1, num_nodes,
                         variable_duration=True)
 t_arr = np.linspace(t0, num_nodes*solution[-1], num_nodes)
 state_sol = CubicSpline(t_arr, state_vals.T)
@@ -304,7 +306,7 @@ coordinates = P_car.pos_from(O).to_matrix(N)
 coordinates = coordinates.row_join(P_wheel.pos_from(O).to_matrix(N))
 
 pL, pL_vals = zip(*par_map.items())
-coords_lam = sm.lambdify(list(state_symbols) + [c, fx, k] + list(pL),
+coords_lam = sm.lambdify(list(state_symbols) + [fx, c, k] + list(pL),
     coordinates, cse=True)
 
 def init_plot():
@@ -313,7 +315,7 @@ def init_plot():
     ax.set_ylim(ymin, ymax)
     ax.set_aspect('equal')
     ax.set_xlabel('x', fontsize=15)
-    ax.set_ylabel('y', fontsize=15)
+    ax.set_ylabel('z', fontsize=15)
 
     # draw the road
     XX = np.linspace(0, 10, 200)
@@ -329,9 +331,9 @@ def init_plot():
     # driving force
     pfeil1 = ax.quiver([], [], [], [], color='green', scale=1500, width=0.004)
     # acceleratrion of the wheel
-    pfeil2 = ax.quiver([], [], [], [], color='blue', scale=750, width=0.004)
+    pfeil2 = ax.quiver([], [], [], [], color='blue', scale=500, width=0.004)
     # acceleration of the body
-    pfeil3 = ax.quiver([], [], [], [], color='magenta', scale=7.5, width=0.004)
+    pfeil3 = ax.quiver([], [], [], [], color='magenta', scale=20, width=0.004)
 
     return fig, ax, line1, line2, line3, pfeil1, pfeil2, pfeil3
 
@@ -341,18 +343,19 @@ def update(t):
     message = (f'running time {t:.2f} sec' +
         f'\n The blue arrow is the ' +
         f'accelerationon the wheel due to uneven street \n' +
-        f'The magenta arrow is the acceleration of the body, magnified 100' +
+        f'The magenta arrow is the acceleration of the body, magnified 25' +
         f' times \n' +
         f'The green arrow is the driving force / video is in slow motion')
     ax.set_title(message, fontsize=12)
 
-    coords = coords_lam(*state_sol(t), *input_sol(t), solution[-2], *pL_vals)
+    coords = coords_lam(*state_sol(t), input_sol(t), solution[-3],
+                        solution[-2], *pL_vals)
     line1.set_offsets([coords[0, 1], coords[2, 1]])
     line2.set_offsets([coords[0, 0], coords[2, 0]])
     line3.set_data([coords[0, 0], coords[0, 1]], [coords[2, 0], coords[2, 1]])
 
     pfeil1.set_offsets([coords[0, 0], coords[2, 0]])
-    pfeil1.set_UVC(input_sol(t)[1], 0)
+    pfeil1.set_UVC(input_sol(t), 0)
 
     pfeil2.set_offsets([coords[0, 1], coords[2, 1]])
     pfeil2.set_UVC(0.0, state_sol(t)[7])
@@ -365,6 +368,6 @@ def update(t):
 # Create the animation.
 fig, ax, line1, line2, line3, pfeil1, pfeil2, pfeil3 = init_plot()
 animation = FuncAnimation(fig, update, frames=np.arange(t0,
-    num_nodes*solution[-1], 1 / fps), interval=5000/fps)
+    num_nodes*solution[-1], 1 / fps), interval=8000/fps)
 
 plt.show()
