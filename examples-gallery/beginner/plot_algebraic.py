67e114e5334901e3dfe1b3fe770b10ee63d9eacb
Jason K. Moore <moorepants@gmail.com>
2025-04-26 18:03:29 +0200

    Added more text to the path constraint example.

--- examples-gallery/beginner/plot_algebraic.py
+++ examples-gallery/beginner/plot_algebraic.py
@@ -2,12 +2,22 @@
 Path Constraints
 ================
 
+Objectives
+----------
+
+- Include additional path constraints in addition to the differential
+  equations.
+
+Introduction
+------------
+
 Given a set of differential equations, any number of additional constraint
 equations that are functions of the states can be appended to restrict the
 trajectory. These are typically called "path constraints". Below the 6 ordinary
 differential equations of motion of a particle moving in space with an applied
 force are given. One algebraic path constraints is added to restrict the
-particle to being on the surface of a cylinder.
+particle to being on the surface of a cylinder, i.e. Pythagoras's theorem
+:math:`x^2 + y^2 = r^2`.
 
 """
 import numpy as np
@@ -43,6 +53,8 @@ num_nodes = 101
 dt = 0.1
 t0, tf = 0.0, dt*(num_nodes - 1)
 
+# %%
+# Minimize the average force magnitude over time.
 obj_func = sm.Integral(Fx**2 + Fy**2 + Fz**2, t)
 obj, obj_grad = create_objective_function(
     obj_func, states, specifieds, tuple(), num_nodes,
@@ -50,7 +62,7 @@ obj, obj_grad = create_objective_function(
 
 # %%
 # Require that the particle make a half turn around the cylinder and rise a
-# specified distance, being stationary at start and stop.
+# specified distance, :math:`4r`, being stationary at start and stop.
 instance_constraints = (
     x.func(t0),
     y.func(t0) + r,
@@ -84,23 +96,20 @@ prob = Problem(
     instance_constraints=instance_constraints,
     time_symbol=t,
     backend='numpy',
-    integration_method='midpoint',
 )
 
-initial_guess = np.random.random(prob.num_free)
-
 # %%
+# Visualize the sparseness of the Jacobian for the non-linear programming
+# problem.
+initial_guess = np.random.random(prob.num_free)
 jac_vals = prob.jacobian(initial_guess)
 row_idxs, col_idxs = prob.jacobianstructure()
 jacobian_matrix = coo_matrix((jac_vals, (row_idxs, col_idxs)))
-
 plt.spy(jacobian_matrix)
 
 # %%
+# Solve the problem.
 solution, info = prob.solve(initial_guess)
-
-# %%
-_ = prob.plot_objective_value()
 
 # %%
 _ = prob.plot_trajectories(solution)
