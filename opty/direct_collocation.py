c936bb2150f477fd51cf85586c9df822891c7447
Peter Stahlecker <peter.stahlecker@gmail.com>
2025-05-09 11:22:43 +0200

    added single EOM, explanation in docstrings

--- opty/direct_collocation.py
+++ opty/direct_collocation.py
@@ -651,7 +651,24 @@ class Problem(cyipopt.Problem):
         - r : number of unknown parameters
         - s : number of unknown time intervals
 
+        If ``eom_bounds`` are given as :math:`a \leq eom \leq b` and
+        ``subplots = True``, the values of the respective eoms are plotted and
+        their bounds are shown as dashed lines.
+
+        if ``eom_bounds`` are given and ``subplots = False``, the eom
+        violations are plotted. The violations are calculated as follows:
+
+        - eom - a if eom < a
+        - eom - b if eom > b
+        - 0 otherwise.
+
+        If only one eom is given and and eom bounds are given, then
+        ``subplots`` is set to ``True``.
+
         """
+        # This is done to make the cases below simpler.
+        if self.collocator.num_eom == 1:
+            subplots = True
 
         bars_per_plot = None
         rotation = -45
@@ -753,9 +770,22 @@ class Problem(cyipopt.Problem):
 
         if subplots is False or self.collocator.num_eom == 1:
             axes[0].plot(con_nodes, eom_violations.T)
-            axes[0].set_title('Constraint violations')
-            axes[0].set_xlabel('Node Number')
-            axes[0].set_ylabel('EoM violation')
+            if self.eom_bounds is None:
+                axes[0].set_title('Constraint violations')
+                axes[0].set_xlabel('Node Number')
+                axes[0].set_ylabel('EoM violation')
+            elif self.collocator.num_eom == 1:
+                axes[0].set_title('Constraint violations')
+                axes[0].set_xlabel('Node Number')
+                axes[0].set_ylabel('EoM value')
+                axes[0].axhline(self.eom_bounds[0][0], color='C1', lw=1.0,
+                                 linestyle='--')
+                axes[0].axhline(self.eom_bounds[0][1], color='C1', lw=1.0,
+                                linestyle='--')
+            else:
+                axes[0].set_title('Constraint violations')
+                axes[0].set_xlabel('Node Number')
+                axes[0].set_ylabel('EoM violations')
 
         else:
             for i in range(self.collocator.num_eom):
@@ -781,7 +811,7 @@ class Problem(cyipopt.Problem):
                 axes[0].set_title('Constraint violations')
             else:
                 axes[0].set_title((f'Constraint violations \n'
-                                                 f'Values of bounde EOMs'))
+                                                 f'Values of bounded EoMs'))
 
 
         if self.collocator.instance_constraints is not None:
