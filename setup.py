e47e49ee734831e36a375f772f69abc5e1eec32c
Jason K. Moore <moorepants@gmail.com>
2025-10-31 08:03:04 +0100

    Support Python 3.14.

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Operating System :: OS Independent',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
